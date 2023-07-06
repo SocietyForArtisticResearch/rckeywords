cc310c0aaba40b0c83d7edfa7e80325c95bdec6d
Casper Schipper <casper.schipper@gmail.com>
2023-07-06 11:52:28 +0200

    More cleanup

--- build/worker.js
+++ build/worker.js
@@ -3,11 +3,9 @@ importScripts("elm-worker.js")
 const app = Elm.Worker.init();
 
 onmessage = function (data) {
-  console.log("onmessage, data",data.data);
   app.ports.searchKeyword.send(data.data);
 };
 
 app.ports.returnResults.subscribe(function(data) {
-	console.log("returning results",data);
     postMessage(data);
 });
